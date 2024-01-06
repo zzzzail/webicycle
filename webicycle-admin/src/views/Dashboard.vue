0a810048b56a4ff0070e9f26e371b757e82c67ab
zhangxq <zzzzail@163.com>
2024-01-06 18:42:10 +0800

    1

--- webicycle-admin/src/views/Dashboard.vue
+++ webicycle-admin/src/views/Dashboard.vue
@@ -2,12 +2,8 @@
     <div class="dashboard">
         <h1>This is an dashboard page</h1>
     </div>
-
-
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/about">About</RouterLink>
-
-    <RouterView />
 </template>
 
 <style>
